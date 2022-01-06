54f274fb81fd974b4459358312b4c9a1291b0931
piginzoo <piginzoo@gmail.com>
2022-01-06 12:23:52 +0800

    bugfix

--- example/testback.py
+++ example/testback.py
@@ -91,11 +91,21 @@ class CombineFactorStrategy(bt.Strategy):
         - 根据每只股票的合成因子值排序，找出最好的100只备选股
         - 卖出持仓的，却不在备选股中的那些股票
         - 买入那些在备选股中，却不在持仓中的股票
+        - TODO：目前不考虑已购入股票的仓位调整，未来考虑
         异常处理：
         - 如果股票停盘，会顺序买入下一位排名的股票
         - 每次都是满仓，即用卖出的股票头寸，全部购入新的股票，头寸仅在新购入股票中平均分配
         - 如果没有头寸，则不再购买（这种情况应该不会出现）
         """
+
+        # logger.debug("--------------------------------------------------")
+        # logger.debug('当前可用资金:%r', self.broker.getcash())
+        # logger.debug('当前总资产:%r', self.broker.getvalue())
+        # logger.debug('当前持仓量:%r', self.broker.getposition(self.data).size)
+        # logger.debug('当前持仓成本:%r', self.broker.getposition(self.data).price)
+        # logger.debug('当前持仓量:%r', self.getposition(self.data).size)
+        # logger.debug('当前持仓成本:%r', self.getposition(self.data).price)
+        # logger.debug("--------------------------------------------------")
 
         # 回测最后一天不进行买卖,datas[0]就是当天, bugfix:  之前self.datas[0].date(0)不行，因为df.index是datetime类型的
         current_date = self.datas[0].datetime.datetime(0)
@@ -108,7 +118,6 @@ class CombineFactorStrategy(bt.Strategy):
         self.current_day = 0
 
         factor = self.factors.loc[current_date]
-
         logger.debug("交易日：%r , %d/%d", utils.date2str(current_date), self.count, self.total)
         if np.isnan(factor).all():
             logger.debug("%r 日的因子全部为NAN，忽略当日", utils.date2str(current_date))
@@ -116,57 +125,109 @@ class CombineFactorStrategy(bt.Strategy):
 
         factor = factor.dropna()
         factor = factor.sort_values(ascending=False)
-        logger.debug("当天的因子为：%r",factor)
+        logger.debug("当天的因子为：%r", factor)
         # 选择因子值前20%
-        select_stocks = factor.index[:math.ceil(0.2*len(factor))]
-        logger.debug("此次选中的股票为：%r",select_stocks)
+        select_stocks = factor.index[:math.ceil(0.2 * len(factor))]
+        logger.debug("此次选中的股票为：%r", select_stocks)
 
         # 以往买入的标的，本次不在标的中，则先平仓
+        # "常规下单函数主要有 3 个：买入 buy() 、卖出 sell()、平仓 close() "
         to_sell_stocks = set(self.current_stocks) - set(select_stocks)
-        for sell_stock in to_sell_stocks:
-            logger.debug('卖出 %s 平仓: %r', sell_stock , self.getposition(sell_stock).size)
-            o = self.close(sell_stock)
-            self.order_list.append(o)  # 记录订单
-            self.current_stocks.remove(sell_stock)
-        logger.debug("合计卖出%d只股票，剩余%d只持仓",len(to_sell_stocks),len(self.current_stocks))
 
+        logger.debug("卖出股票：%r", to_sell_stocks)
+        for sell_stock in to_sell_stocks:
+            # 根据名字获得对应那只股票的数据
+            stock_data = self.getdatabyname(sell_stock)
+            self.close(data=stock_data)
+            self.current_stocks.remove(sell_stock)
+            logger.debug('卖出 %s 平仓: %r', sell_stock, self.getposition(sell_stock).size)
+        logger.debug("合计卖出%d只股票，剩余%d只持仓", len(to_sell_stocks), len(self.current_stocks))
 
         # 每只股票买入资金百分比，预留2%的资金以应付佣金和计算误差
         buy_percentage = (1 - 0.02) / len(select_stocks)
 
-        # 得到目标市值
-        targetvalue = buy_percentage * self.broker.getvalue()
+        # 得到可以用来购买的金额
+        buy_amount = buy_percentage * self.broker.getvalue()
 
-        next_trade_day = self.datas[0].datetime(1)  # 下一交易日
         for buy_stock in select_stocks:
+
+            # 如果选中的股票在当前的持仓中，就忽略
             if buy_stock in self.current_stocks:
                 logger.debug("%s 在持仓中，不动", buy_stock)
                 continue
 
-            # 按次日开盘价计算下单量，下单量是100的整数倍
-            size = int(
-                abs((self.broker.getvalue([d]) - targetvalue) / d.open[1] // 100 * 100))
-            if self.broker.getvalue([d]) > targetvalue:  # 持仓过多，要卖
-                # 次日跌停价近似值
-                lowerprice = d.close[0] * 0.9 + 0.02
+            # 根据名字获得对应那只股票的数据
+            stock_data = self.getdatabyname(buy_stock)
+            open_price = stock_data.open[0]
+            # 按次日开盘价计算下单量，下单量是100（手）的整数倍
+            size = math.ceil(buy_amount / open_price)
+            logger.debug("购入股票[%s 股价%.2f] %d股，金额:%.2f", buy_stock, open_price, size, buy_amount)
+            self.buy(data=stock_data, size=size, price=open_price, exectype=bt.Order.Limit)
+            self.current_stocks.append(buy_stock)
 
-                o = self.sell(data=d, size=size, exectype=bt.Order.Limit,
-                              price=lowerprice, valid=validday)
-            else:  # 持仓过少，要买
-                # 次日涨停价近似值
-                upperprice = d.close[0] * 1.1 - 0.02
-                o = self.buy(data=d, size=size, exectype=bt.Order.Limit,
-                             price=upperprice, valid=validday)
+    # 记录交易执行情况（可省略，默认不输出结果）
+    def notify_order(self, order):
+        # logger.debug('订单状态：%r', order.Status[order.status])
+        # print(order)
 
-            self.order_list.append(o)  # 记录订单
+        # 如果order为submitted/accepted,返回空
+        if order.status in [order.Submitted, order.Accepted]:
+            return
+
+        # 如果order为buy/sell executed,报告价格结果
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                logger.debug('买入: 价格[%.2f],成本[%.2f],手续费[%.2f]',
+                             order.executed.price,
+                             order.executed.value,
+                             order.executed.comm)
+
+                self.buyprice = order.executed.price
+                self.buycomm = order.executed.comm
+            else:
+                logger.debug('卖出: 价格[%.2f],成本[%.2f],手续费[%.2f]',
+                             order.executed.price,
+                             order.executed.value,
+                             order.executed.comm)
+
+            self.bar_executed = len(self)
+
+        # 如果指令取消/交易失败, 报告结果
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            """
+            Order.Created：订单已被创建；
+            Order.Submitted：订单已被传递给经纪商 Broker；
+            Order.Accepted：订单已被经纪商接收；
+            Order.Partial：订单已被部分成交；
+            Order.Complete：订单已成交；
+            Order.Rejected：订单已被经纪商拒绝；
+            Order.Margin：执行该订单需要追加保证金，并且先前接受的订单已从系统中删除；
+            Order.Cancelled (or Order.Canceled)：确认订单已经被撤销；
+            Order.Expired：订单已到期，其已经从系统中删除 。
+            """
+            logger.debug('交易失败，订单状态：%r', order.Status[order.status])
+
+        self.order = None
+
+    # 记录交易收益情况（可省略，默认不输出结果）
+    def notify_trade(self, trade):
+        if not trade.isclosed:
+            return
+        logger.debug('策略收益：毛收益 [%.2f], 净收益 [%.2f]', trade.pnl, trade.pnlcomm)
 
 
+# # 自定义数据
+# class FactorData(PandasData):
+#     lines = ('market_value', 'momentum', 'peg', 'clv',)
+#     params = (('market_value', 7), ('momentum', 8), ('peg', 9), ('clv', 10),)
 
 
-# 修改原数据加载模块，以便能够加载更多自定义的因子数据
-class FactorData(PandasData):
-    lines = ('market_value', 'momentum', 'peg', 'clv',)
-    params = (('market_value', 7), ('momentum', 8), ('peg', 9), ('clv', 10),)
+# 按照backtrader要求的数据格式做数据整理,格式化成backtrader要求：索引日期；列名叫vol和datetime
+def comply_backtrader_data_format(df):
+    df['trade_date'] = pd.to_datetime(df['trade_date'], format="%Y%m%d")
+    df = df.rename(columns={'vol': 'volume', 'trade_date': 'datetime'})  # 列名准从backtrader的命名规范
+    df = df.set_index('datetime')
+    return df
 
 
 def main(start_date, end_date, index_code, period, stock_num):
@@ -181,33 +242,37 @@ def main(start_date, end_date, index_code, period, stock_num):
 
     cerebro = bt.Cerebro()  # 初始化cerebro
 
-    # 加载指数，就是为了当日期占位符
-    df_index = tushare_utils.index_daily("000001.SH", start_date, end_date)
-
     stock_codes = tushare_utils.index_weight(index_code, start_date)
     stock_codes = stock_codes[:stock_num]
 
     combined_factor = factor_combiner.synthesize_by_jaqs(stock_codes, start_date, end_date)
     combined_factor.index = pd.to_datetime(combined_factor.index, format="%Y%m%d")
-    logger.debug("合成的多因子为：\n%r",combined_factor)
+    logger.debug("合成的多因子为：\n%r", combined_factor)
 
-    start_date = utils.str2date(start_date)  # 开始日期
-    end_date = utils.str2date(end_date)  # 结束日期
+    d_start_date = utils.str2date(start_date)  # 开始日期
+    d_end_date = utils.str2date(end_date)  # 结束日期
 
-    # 格式化成backtrader要求：索引日期；列名叫vol和datetime
-    df_index['trade_date'] = pd.to_datetime(df_index['trade_date'], format="%Y%m%d")
-    df_index = df_index.rename(columns={'vol': 'volume', 'trade_date': 'datetime'})
-    df_index = df_index.set_index('datetime')
-
-    data = PandasData(dataname=df_index, fromdate=start_date, todate=end_date)
-    # 在Cerebro中添加股票数据
+    # 加载上证指数，就是为了当日期占位符,在Cerebro中添加上证股指数据,格式: datetime,open,high,low,close,volume,openi..
+    df_index = tushare_utils.index_daily("000001.SH", start_date, end_date)
+    df_index = comply_backtrader_data_format(df_index)
+    data = PandasData(dataname=df_index, fromdate=d_start_date, todate=d_end_date)
     cerebro.adddata(data, name="000001.SH")
+    logger.debug("初始化上证数据到脑波：%d 条", len(df_index))
 
-    ################## cerebro整体设置 #####################
+    # 想脑波cerebro逐个追加每只股票的数据
+    for stock_code in stock_codes:
+        df_stock = tushare_utils.daily(stock_code, start_date, end_date)
+        df_stock = comply_backtrader_data_format(df_stock)
+        data = PandasData(dataname=df_stock, fromdate=d_start_date, todate=d_end_date)
+        cerebro.adddata(data, name=stock_code)
+        logger.debug("初始化股票[%s]数据到脑波cerebro：%d 条", stock_code, len(df_stock))
+    logger.debug("合计追加 %d 只股票数据到脑波cerebro", len(stock_codes) + 1)
+
+    ################## cerebro 整体设置 #####################
 
     # 设置启动资金
-    startcash = 100000.0
-    cerebro.broker.setcash(startcash)
+    start_cash = 100000.0
+    cerebro.broker.setcash(start_cash)
 
     # https://baike.baidu.com/item/%E8%82%A1%E7%A5%A8%E4%BA%A4%E6%98%93%E6%89%8B%E7%BB%AD%E8%B4%B9/9080806
     # 设置交易手续费：印花税0.001+证管费0.00002+证券交易经手费0.0000487+券商交易佣金0.003
@@ -216,7 +281,7 @@ def main(start_date, end_date, index_code, period, stock_num):
     # 设置订单份额,设置每笔交易交易的股票数量,比如" cerebro.addsizer(bt.sizers.FixedSize, stake=10)"
     # 告诉平台，我们是每次买卖股票数量固定的，stake=10就是10股。
     # 实际过程中，我们不可能如此简单的制定买卖的数目，而是要根据一定的规则，这就需要自己写一个sizers
-    cerebro.addsizer(Percent)
+    # cerebro.addsizer(Percent)
 
     # 将交易策略加载到回测系统中
     cerebro.addstrategy(CombineFactorStrategy, index, period, len(df_index), combined_factor)
@@ -226,12 +291,12 @@ def main(start_date, end_date, index_code, period, stock_num):
     cerebro.addanalyzer(bt.analyzers.DrawDown, _name='DW')  # 回撤分析
 
     # 打印
-    logger.debug('回测期间：%r ~ %r , 初始资金: %r', start_date, end_date, startcash)
+    logger.debug('回测期间：%r ~ %r , 初始资金: %r', start_date, end_date, start_cash)
     # 运行回测
     results = cerebro.run()
     # 打印最后结果
     portvalue = cerebro.broker.getvalue()
-    pnl = portvalue - startcash
+    pnl = portvalue - start_cash
     # 打印结果
     logger.debug(f'总资金: {round(portvalue, 2)}')
     logger.debug(f'净收益: {round(pnl, 2)}')

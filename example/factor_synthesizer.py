9e6746bef00a248f09c4642ed53ac4c73349a748
piginzoo <piginzoo@gmail.com>
2022-02-12 21:51:45 +0800

    refactor factors synthesize

--- example/factor_synthesizer.py
+++ example/factor_synthesizer.py
@@ -7,7 +7,6 @@ from jaqs_fxdayu.research.signaldigger import multi_factor
 
 from datasource import datasource_factory, datasource_utils
 from example import factor_utils
-from temp import multifactor_synthesize
 from utils import utils
 
 logger = logging.getLogger(__name__)
@@ -28,18 +27,7 @@ factor_synth_clv_mv_ic_weight
 """
 
 
-def synthesize(stock_codes, start_date, end_date):
-    """测试因子合成"""
-    factors = {}
-    for factor_key in factor_utils.FACTORS.keys():
-        factors[factor_key] = factor_utils.get_factor(factor_key, stock_codes, start_date, end_date)
-
-    logger.debug("开始合成因子：%r", factors.keys())
-    combined_factor = multifactor_synthesize.synthesize(factors, None)
-    return combined_factor
-
-
-def synthesize_by_jaqs(stock_codes, factor_dict, start_date, end_date, weight="ic_weight"):
+def synthesize_by_jaqs(stock_codes, factor_dict, df_index_daily, start_date, end_date, weight="ic_weight"):
     """
     测试因子合成，要求数据得是panel格式的，[trade_date,stock1,stock2,....]
     """
@@ -49,28 +37,23 @@ def synthesize_by_jaqs(stock_codes, factor_dict, start_date, end_date, weight="i
 
     df_stocks = datasource.daily(list(stock_codes), start_date, end_date)
     df_stocks = datasource_utils.reset_index(df_stocks)
+
     # unstack将行转化成列
     __prices = df_stocks['close'].unstack()
     __highs = df_stocks['high'].unstack()
     __lows = df_stocks['low'].unstack()
 
-    zz500 = datasource.index_daily('000905.SH', start_date, end_date)
-    zz500 = datasource_utils.reset_index(zz500)
-    zz500 = zz500['close'].pct_change(1)
-    zz500 = factor_utils.to_panel_of_stock_columns(zz500)
-    assert len(zz500) != 0
-
     logger.debug("close价格：%d 条,索引：%r", len(__prices), list(__prices.index.names))
     logger.debug("high价格：%d 条,索引：%r", len(__highs), __highs.index.names)
     logger.debug("low价格：%d 条,索引：%r", len(__lows), __lows.index.names)
-    logger.debug("中证价格：%d 条,索引：%r", len(zz500), zz500.index.names)
+    logger.debug("指数价格：%d 条,索引：%r", len(df_index_daily), df_index_daily.index.names)
 
     props = {
         'price': __prices,
         'high': __highs,  # 可为空
         'low': __lows,  # 可为空
         'ret_type': 'return',  # 可选参数还有upside_ret/downside_ret 则组合因子将以优化潜在上行、下行空间为目标
-        'daily_benchmark_ret': zz500,  # 为空计算的是绝对收益　不为空计算相对收益
+        'daily_benchmark_ret': df_index_daily,  # 为空计算的是绝对收益　不为空计算相对收益
         'period': 10,  # 30天的持有期
         'forward': True,
         'commission': 0.0008,
@@ -94,9 +77,16 @@ def main(start_date, end_date, index_code, stock_num, factor_names, weight):
 
     factor_dict = {}
     for factor_name in factor_names:
-        df_factor = factor_utils.get_factor(stock_codes, None, start, end)
+        df_factor = factor_utils.get_factor(factor_names, stock_codes, start_date, end_date)
         factor_dict[factor_name] = df_factor
-    df_combined_factor = synthesize_by_jaqs(stock_codes, factor_dict, start, end, weight)
+
+    df_index_daily = datasource.index_daily(index_code, start_date, end_date)
+    df_index_daily = datasource_utils.reset_index(df_index_daily)
+    df_index_daily = df_index_daily['close'].pct_change(1)
+    df_index_daily = factor_utils.to_panel_of_stock_columns(df_index_daily)
+    assert len(df_index_daily) != 0
+
+    df_combined_factor = synthesize_by_jaqs(stock_codes, factor_dict, df_index_daily, start_date, end_date, weight)
 
     print(df_combined_factor.head(3))
 
@@ -113,6 +103,7 @@ python -m example.factor_synthesizer \
     --start 20170101 \
     --end 20180101 \
     --num 20 \
+    --weight ic_weight \
     --index 000905.SH
 """
 
@@ -130,24 +121,16 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--start', type=str, help="开始日期")
     parser.add_argument('-e', '--end', type=str, help="结束日期")
     parser.add_argument('-i', '--index', type=str, help="股票池code")
-    parser.add_argument('-p', '--period', type=str, help="调仓周期，多个的话，用逗号分隔")
     parser.add_argument('-n', '--num', type=int, help="股票数量")
     args = parser.parse_args()
 
-    if "," in args.period:
-        periods = [int(p) for p in args.period.split(",")]
-    else:
-        periods = [int(args.period)]
+    if "," not in args.factor: raise ValueError("请提供多个因子，逗号分割 : " + args.factor)
 
-    if "," in args.factor:
-        factors = [int(p) for p in args.factor.split(",")]
-    else:
-        factors = [int(args.factor)]
+    factors = [p for p in args.factor.split(",")]
 
     main(args.start,
          args.end,
          args.index,
-         periods,
          args.num,
          factors,
          args.weight)

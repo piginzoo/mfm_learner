5a51054f2c150872d1d703a6bc4f92c151ae2e57
piginzoo <piginzoo@gmail.com>
2022-08-10 11:42:31 +0800

    download all

--- mfm_learner/utils/tushare_download/updator.py
+++ mfm_learner/utils/tushare_download/updator.py
@@ -1,6 +1,14 @@
 """
 用来，一键更新所有的新数据到本地，省的每个还都得再下载
 """
+from mfm_learner.utils.tushare_download.downloaders.index_weekly import IndexWeekly
+
+from mfm_learner.utils.tushare_download.downloaders.cashflow import CashFlow
+
+from mfm_learner.utils.tushare_download.downloaders.income import Income
+
+from mfm_learner.utils.tushare_download.downloaders.balancesheet import BalanceSheet
+
 from mfm_learner.utils import utils
 from mfm_learner.utils.tushare_download.downloaders.daily import Daily
 from mfm_learner.utils.tushare_download.downloaders.daily_basic import DailyBasic
@@ -8,6 +16,7 @@ from mfm_learner.utils.tushare_download.downloaders.daily_hfq import DailyHFQ
 from mfm_learner.utils.tushare_download.downloaders.fina_indicator import FinanceIndicator
 from mfm_learner.utils.tushare_download.downloaders.index_daily import IndexDaily
 from mfm_learner.utils.tushare_download.downloaders.index_weight import IndexWeight
+from mfm_learner.utils.tushare_download.downloaders.stk_holdernumber import StockHolderNumber
 from mfm_learner.utils.tushare_download.downloaders.stock_basic import StockBasic
 from mfm_learner.utils.tushare_download.downloaders.stock_company import StockCompany
 from mfm_learner.utils.tushare_download.downloaders.trade_cal import TradeCalendar
@@ -17,7 +26,6 @@ logger = logging.getLogger(__name__)
 
 
 def main():
-    start = time.time()
     TradeCalendar().download()
     StockCompany().download()
     StockBasic().download()
@@ -28,11 +36,19 @@ def main():
 
 
 def download_all():
+    start = time.time()
+
     main()
 
     # 目前这2个数据不需要使用
     FinanceIndicator().download()
     IndexWeight(["000001.SH", "000905.SH", "000300.SH", "000016.SH"]).download()
+    BalanceSheet().download()
+    Income().download()
+    CashFlow().download()
+    BalanceSheet().download()
+    IndexWeekly(["000001.SH", "000905.SH", "000300.SH", "000016.SH"]).download()
+    StockHolderNumber().downlaod()
 
     logger.debug("下载所有的最新数据，共耗时: %s ", str(datetime.timedelta(seconds=time.time() - start)))
 

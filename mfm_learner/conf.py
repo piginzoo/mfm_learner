f954df8fc0917cb790ead7666a8e8b7b7cdd4a3b
piginzoo <piginzoo@gmail.com>
2022-08-19 10:54:55 +0800

    bugfix

--- mfm_learner/conf.py
+++ mfm_learner/conf.py
@@ -1,4 +1,4 @@
-CONF_PATH = "../conf/config.yml"
-DATA_DIR = "../data"
+CONF_PATH = "./conf/config.yml"
+DATA_DIR = "./data"
 BAR_DIR = DATA_DIR + "/bar"
 DATE_COLUMNS=['trade_date','datetime','date','ann_date']
